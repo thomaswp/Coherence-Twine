8c48cd075edbebfa4f7d020f2631d41f5ce3b185
Thomas Price <thomaswprice1@gmail.com>
2025-07-13 22:29:17 -0400

    PoC

--- src/story.js
+++ src/story.js
@@ -18,3 +18,53 @@ $(document).on(':storyready', function () {
 	}
     hasLoaded = true;
 });
+
+function gameInit() {
+	State.variables.isDoorLocked = true;
+    State.variables.time = 0;
+}
+
+
+function getPreservedVars() {
+	const out = {};
+	for (const key in State.variables) {
+		if (key.startsWith("persist")) {
+			out[key] = State.variables[key];
+		}
+	}
+	return out;
+}
+
+
+function softReset() {
+	// Cache preserved values
+	const preserved = getPreservedVars();
+
+	// Replace entire variable store
+	for (const key in State.variables) {
+		delete State.variables[key]
+	}
+
+	// Restore preserved values
+	for (const key in preserved) {
+		State.variables[key] = preserved[key];
+	}
+
+	// Re-run init
+	gameInit();
+
+	// Optionally go to a specific passage
+	Engine.play("Entryway");
+}
+
+Macro.add("soft-reset", {
+	handler() {
+		softReset();
+	}
+});
+
+Macro.add("init-vars", {
+    handler() {
+        gameInit();
+    }
+});
